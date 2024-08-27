12eb8b3d00beaba0fe4cd882e6ff11beda4e512e
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2024-08-26 22:03:50 -0400

    大致做了泡泡图

--- components/position/scene-com/ScenCardCom.tsx
+++ components/position/scene-com/ScenCardCom.tsx
@@ -3,15 +3,12 @@ import BubbleChart from "@weknow/react-bubble-chart-d3";
 
 const BubbleChartComponent = ({ obj }: any) => {
   return (
-    <div className="w-full h-[40rem]">
       <BubbleChart
         graph={{
           zoom: 1,
-          offsetX: -0.2,  // 使用数字类型
+          offsetX: 0,  // 使用数字类型
           offsetY: 0,   // 使用数字类型
         }}
-        width={Number(obj.style.diameter) * 5}  // 使用数字类型
-        height={Number(obj.style.diameter) * 5} // 使用数字类型
         padding={2}
         showLegend={false}
         valueFont={{
@@ -26,11 +23,8 @@ const BubbleChartComponent = ({ obj }: any) => {
           color: "#fff",
           weight: "bold",
         }}
-        data={[
-          { label: obj.label, value:obj.style.diameter, color: "blue"}
-        ]}
+        data={obj}
       />
-    </div>
   );
 };
 
--- components/position/scene-com/SceneCom.tsx
+++ components/position/scene-com/SceneCom.tsx
@@ -6,13 +6,8 @@ import BubbleChartComponent from "./ScenCardCom";
 const SceneCom = () => {
   return (
     <div className="w-full h-auto flex justify-center items-center">
-    <div className="w-full h-[40rem] bg-white flex justify-center items-center relative">
-      {
-        content.scenearr.map((item,index)=>{
-          return <BubbleChartComponent key={index} obj={item}/>
-        })
-      }
-      
+    <div className="w-full h-full bg-white flex justify-center items-center relative">
+      <BubbleChartComponent obj={content.scenarioArr}/>
     </div>
   </div>
   
